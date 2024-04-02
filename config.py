4372d9300ce2e7467dc4d89e39ef11b4d6e8aedf
akpandawep <154840412+akpandawep@users.noreply.github.com>
2024-04-02 19:49:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # pyro client config
-    API_ID    = os.environ.get("API_ID", "23941369") #⚠️ Required
-    API_HASH  = os.environ.get("API_HASH", "bbd37235e41a95d03bc144ea6bc7b2cd") #⚠️ Required
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7180148674:AAG4gO8_GTGBbIqq38yS7OgYfkHXDvw7Cb4") #⚠️ Required
+    API_ID    = os.environ.get("API_ID", "22287041") #⚠️ Required
+    API_HASH  = os.environ.get("API_HASH", "c149386dcd58a40fa9fe60e632e161d4") #⚠️ Required
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6590068990:AAG1uI2CHAvISuEBnyZAzQ5jwpbGQFtohPc") #⚠️ Required
     
     #premium client
-    STRING = os.environ.get("STRING", "") #⚠️ Required 
-    STRING_API_ID = os.environ.get("STRING_API_ID", "23941369") # ⚠️ Required
-    STRING_API_HASH = os.environ.get("STRING_API_HASH", "bbd37235e41a95d03bc144ea6bc7b2cd") # ⚠️ Required
+    STRING = os.environ.get("STRING", "BQDa-FcAUUgvpOJGhG1jCLDDyOfLofOcw_21dQUUKWhhAu1DSxawig4rSftvpTskthbf_wCdKk-xpISCPMgaPNltTqC7QUch8I-fXkwCMHSuj84puhV0LRBGVSHRLXgWwppW1uyLYKQ9fkPN8lkpP-M8-mDHIV3nvTdP5-3DUQaS5trFAamM0JWBS-S39TGX89Som6Ex-2DcjckPCmPNvjA7WiyrX4pewKKlfrvbU_44UZxFEQq1NbHIDX07f--adYmQ6SuDMfWYaA6lQx599fr4HwGhaRgWjdYnQkKeETwpLwFZCzX3LSKQZlHsnTtgREvkXb6EVh7gvGQOQnvc2fkyb3kRyAAAAAGJzmhsAA") #⚠️ Required 
+    STRING_API_ID = os.environ.get("STRING_API_ID", "22287041") # ⚠️ Required
+    STRING_API_HASH = os.environ.get("STRING_API_HASH", "c149386dcd58a40fa9fe60e632e161d4") # ⚠️ Required
    
     # database config
     DB_NAME = os.environ.get("DB_NAME","Premium_Data")     
@@ -19,9 +19,9 @@ class Config(object):
  
     # other configs
     BOT_UPTIME  = time.time()
-    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '1966867320').split()] #⚠️ Required
+    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6141937812').split()] #⚠️ Required
     FORCE_SUB   = os.environ.get("FORCE_SUB", "Rokubotz") #⚠️ Required Username without @
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002046934698")) #⚠️ Required
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001869105126")) #⚠️ Required
     FLOOD = int(os.environ.get("FLOOD", '10'))
     BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 
